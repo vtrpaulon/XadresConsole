b77304acf2621f565200f77ef8cbd8cae4e44485
VITOR .. <vitorpaulon@gmail.com>
2024-12-25 13:38:39 -0300

    TratandoErros

--- Program.cs
+++ Program.cs
@@ -9,15 +9,20 @@ namespace XadresConsole
     {
         static void Main(string[] args)
         {
-            Tabuleiro tabuleiro = new Tabuleiro(8, 8);
+            try
+            {
+                Tabuleiro tabuleiro = new Tabuleiro(8, 8);
             
-            tabuleiro.colocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(0,0));
-            tabuleiro.colocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(1,3));
-            tabuleiro.colocarPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(2,4));
+                tabuleiro.colocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(0,0));
+                tabuleiro.colocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(1,3));
+                tabuleiro.colocarPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(2,4));
 
-
-            Tela.imprimirTabuleiro(tabuleiro);
-
+                Tela.imprimirTabuleiro(tabuleiro);
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
             Console.ReadLine();
         }
     }
---
+++ tabuleiro/TabuleiroException.cs
@@ -0,0 +1,9 @@
+namespace XadresConsole.tabuleiro;
+
+public class TabuleiroException : Exception
+{
+    public TabuleiroException(string msg) : base(msg)
+    {
+        
+    }
+}
